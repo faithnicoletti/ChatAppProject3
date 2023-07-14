de412caa84974271461455b902a492fb3eaeaa8d
faithnicoletti <faithalicia22@gmail.com>
2023-07-14 10:26:20 -0400

    working home pagee/some css

--- ChatAppProject3/main_app/models.py
+++ ChatAppProject3/main_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=100)
+
+class Chat(models.Model):
+    content = models.TextField(max_length=600)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    participants = models.ManyToManyField(User)
+
+def __str__(self):
+    return f"Chat {self.id}"
--- ChatAppProject3/main_app/views.py
+++ ChatAppProject3/main_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from .models import Chat
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+# class login(request):
+#     return render(request, 'login.html')
