ca5925a13f1c3f39e8972e70acdb12324717f462
shakiba333 <shakibaabdulrashid@gmail.com>
2023-07-14 13:08:50 -0400

    login and signup

---
+++ ChatAppProject3/main_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+
+    path('accounts/signup/', views.signup, name='signup'),
+    path('profile/', views.profile, name='profile'),
+
+
+]
