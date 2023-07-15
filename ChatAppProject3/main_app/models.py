9d62abcfbd969251019a09683aa180e46d782cc2
shakiba333 <shakibaabdulrashid@gmail.com>
2023-07-14 22:21:00 -0400

    Chat model

--- ChatAppProject3/main_app/models.py
+++ ChatAppProject3/main_app/models.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-# class Chat(models.Model):
-#     content = models.TextField(max_length=600)
-#     timestamp = models.DateTimeField(auto_now_add=True)
+class Chat(models.Model):
+    content = models.TextField(max_length=600)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
 
-# def __str__(self):
-#     return f"Chat {self.id}"
+def __str__(self):
+    return f"Chat {self.id}"
